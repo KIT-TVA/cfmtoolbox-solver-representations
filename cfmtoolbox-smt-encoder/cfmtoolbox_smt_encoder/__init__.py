c08efe80ca283ab2045a333521c893afc54b422f
upskk <upskk@student.kit.edu>
2024-11-13 14:11:28 +0100

    fixed multiset encoding and added call to solver

--- cfmtoolbox-smt-encoder/cfmtoolbox_smt_encoder/__init__.py
+++ cfmtoolbox-smt-encoder/cfmtoolbox_smt_encoder/__init__.py
@@ -1,12 +1,36 @@
+import os
+import sys
 
 from cfmtoolbox import app, CFM
 from cfmtoolbox_smt_encoder.mulitsetSMT import create_smt_multiset_encoding
+import subprocess
+
+
 
 
 @app.command()
 def encode_to_smt_multiset(cfm: CFM) -> str:
     encoding = create_smt_multiset_encoding(cfm)
     return encoding
+
+@app.command()
+def run_smtsolver_with_multisetencoding(cfm: CFM):
+    path = os.path.join(os.path.abspath(sys.path[0]), "../../z3/z3/build/z3")
+    cmd = [path ,'-in','-smt2']
+    encoding = create_smt_multiset_encoding(cfm)
+    encoding += "(maximize Feature_salami)"
+    encoding += "(check-sat)"
+    encoding += "(get-model)"
+    encoding += "(exit)"
+    p = subprocess.run(cmd, stdout=subprocess.PIPE,
+                       input=encoding, encoding='ascii')
+
+    print(p.stdout)
+    #p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    #out = p.communicate(input=encoding)
+    #result = out.split('\n')
+    #for lin in result:
+    #    print(lin)
 
 
 @app.command()
--- cfmtoolbox-smt-encoder/cfmtoolbox_smt_encoder/mulitsetSMT.py
+++ cfmtoolbox-smt-encoder/cfmtoolbox_smt_encoder/mulitsetSMT.py
@@ -10,8 +10,8 @@ def create_smt_multiset_encoding(cfm: CFM):
     encoding += declare_constants(cfm.features)
     encoding += create_assert_child_parent_connection(cfm.root.children)
     encoding += create_assert_feature_group_type_cardinality(cfm.root)
-    encoding += create_assert_feature_group_instance_cardinality(cfm.root, Interval(1, 1))
-    encoding += create_assert_feature_instance_cardinality(cfm.root, Interval(1, 1))
+    encoding += create_assert_feature_group_instance_cardinality(cfm.root)
+    encoding += create_assert_feature_instance_cardinality(cfm.root, [Interval(1, 1)])
     encoding += create_assert_constraints(cfm.constraints)
 
     print(encoding)
@@ -62,11 +62,11 @@ def create_assert_constraints(constraints):
         assertStatement += create_constraint_feature_to_intervals(constraint.second_cardinality.intervals, constraint.second_feature)
 
         if not constraint.require:
-            assertStatement += " )"
+            assertStatement += " )" # closing not
 
         assertStatement += "(= true true)"
-        assertStatement += ")"
-        assertStatement += ")"
+        assertStatement += ")" #closing ite
+        assertStatement += ")\n" # closing assert
     return assertStatement
 
 
@@ -78,42 +78,50 @@ def create_constraint_feature_to_intervals(intervals: list, feature: Feature):
         constraint = "(and "
         constraint += "(<= "
         constraint += create_const_name(feature)
+        constraint += " "
         constraint += str(interval.upper)
         constraint += ")"
         constraint += "(>= "
         constraint += create_const_name(feature)
+        constraint +=  " "
         constraint += str(interval.lower)
         constraint += ")"
-
+        constraint += ")" # closing and
     if len(intervals) > 1:
-        constraint += ")"
+        constraint += ")" #closing or
     return constraint
 
-def create_assert_feature_instance_cardinality(feature: Feature, parent: Interval):
-    assertStatement = ""
-    assertStatement += "(assert "
-    if len(feature.instance_cardinality.intervals) > 1:
-        assertStatement += "(or"
-    for interval in feature.instance_cardinality.intervals:
-        assertStatement += "(and "
-        assertStatement += "(>= "
-        assertStatement += create_const_name(feature) + " "
-        assertStatement += str(interval.lower * parent.lower)
-        assertStatement += ") "
-        assertStatement += "(<= "
-        assertStatement += create_const_name(feature) + " "
-        assertStatement += str(interval.upper *  parent.upper)
-        assertStatement += ") "
-        assertStatement += ")"
+def create_assert_feature_instance_cardinality(feature: Feature, parent_intervals: list[Interval]):
+    assert_statement = ""
+    assert_statement += "(assert "
+    if len(parent_intervals) > 1:
+        assert_statement += "(or"
+    for parentInterval in parent_intervals:
+        if len(feature.instance_cardinality.intervals) > 1:
+            assert_statement += "(or"
+        for interval in feature.instance_cardinality.intervals:
+            assert_statement += "(and "
+            assert_statement += "(>= "
+            assert_statement += create_const_name(feature) + " "
+            assert_statement += str(interval.lower * parentInterval.lower)
+            assert_statement += ") "
+            assert_statement += "(<= "
+            assert_statement += create_const_name(feature) + " "
+            assert_statement += str(interval.upper *  parentInterval.upper)
+            assert_statement += ") "
+            assert_statement += ")" # closing and
 
-    if len(feature.group_type_cardinality.intervals) > 1:
-        assertStatement += ")"  # closing or
+        if len(feature.instance_cardinality.intervals) > 1:
+            assert_statement += ")"  # closing or
+    if len(parent_intervals) > 1:
+        assert_statement += ")"
+    assert_statement += ")\n" # closing assert
 
-    for interval in feature.instance_cardinality.intervals:
-        for child in feature.children:
-            assertStatement += create_assert_feature_instance_cardinality(child, interval)
 
-    return assertStatement
+    for child in feature.children:
+        assert_statement += create_assert_feature_instance_cardinality(child, feature.instance_cardinality.intervals)
+
+    return assert_statement
 
 def create_amount_of_children_for_group_type_cardinality(features: list):
     amount = ""
@@ -124,8 +132,8 @@ def create_amount_of_children_for_group_type_cardinality(features: list):
         amount += "(>= " + create_const_name(feature) + " 1)"
         amount += " 1 "
         amount += " 0 "
-        amount += " )"
-    amount += " )"
+        amount += " )" # closing ite
+    amount += " )" # closing +
 
     return amount
 
@@ -137,38 +145,46 @@ def create_sum_of_children_for_group_type_cardinality(features: list):
     for feature in features:
         sum += create_const_name(feature)
         sum += " "
-    sum += " )"
+    sum += " )" # closing +
 
     return sum
 
 
-def create_assert_feature_group_instance_cardinality(feature: Feature, parentCardinality: Interval):
-    assertStatement = ""
+def create_assert_feature_group_instance_cardinality(feature: Feature):
+    assert_statement = ""
     if feature.group_instance_cardinality.intervals:
-        assertStatement += "(assert "
+        assert_statement += "(assert "
+
         if len(feature.group_instance_cardinality.intervals) > 1:
-            assertStatement += "(or"
+            assert_statement += "(or"
         for interval in feature.group_instance_cardinality.intervals:
-            assertStatement += "(and "
-            assertStatement += "(>= "
-            assertStatement += create_sum_of_children_for_group_type_cardinality(feature.children)
-            assertStatement += " "
-            assertStatement += str(interval.lower * parentCardinality.lower)
-            assertStatement += ") "
-            assertStatement += "(<= "
-            assertStatement += create_sum_of_children_for_group_type_cardinality(feature.children)
-            assertStatement += " "
-            assertStatement += str(interval.upper * parentCardinality.upper)
-            assertStatement += ") "
+            assert_statement += "(and "
+            assert_statement += "(>= "
+            assert_statement += create_sum_of_children_for_group_type_cardinality(feature.children)
+            assert_statement += " "
+            if feature.parent:
+                assert_statement += "(* " + str(interval.lower) + " " +  create_const_name(feature.parent) + ")"   #str(interval.lower * parentInterval.lower)
+            else:
+                assert_statement += str(interval.lower)
+            assert_statement += ") "
+            assert_statement += "(<= "
+            assert_statement += create_sum_of_children_for_group_type_cardinality(feature.children)
+            assert_statement += " "
+            if feature.parent:
+                assert_statement += "(* " + str(interval.upper) + " " + create_const_name(feature.parent) + ")"  # str(interval.upper * parentInterval.upper)
+            else:
+                assert_statement += str(interval.upper)
+            assert_statement += ") "
+            assert_statement += ")" # closing and
 
         if len(feature.group_instance_cardinality.intervals) > 1:
-            assertStatement += ")"  # closing or
+            assert_statement += ")"  # closing or
+        assert_statement += ")\n" # closing assert
 
-    for interval in feature.group_instance_cardinality.intervals:
-        for child in feature.children:
-            assertStatement += create_assert_feature_group_instance_cardinality(child, interval)
+    for child in feature.children:
+        assert_statement += create_assert_feature_group_instance_cardinality(child)
 
-    return assertStatement
+    return assert_statement
 
 
 def create_assert_child_parent_connection(features: list) -> str:
@@ -181,7 +197,7 @@ def create_assert_child_parent_connection(features: list) -> str:
             for feature in features:
                 childrenAssert += create_assert_child_parent_connection(feature.children)
 
-            childrenAssert += " )"
+            childrenAssert += " )" # closing and
         else:
             childrenAssert += "(and "
             for feature in features:
@@ -189,13 +205,13 @@ def create_assert_child_parent_connection(features: list) -> str:
                 childrenAssert += "(= " + create_const_name(feature.parent) + " 0)"
                 childrenAssert += "(= " + create_const_name(feature) + " 0)"
                 childrenAssert += "(>= " + create_const_name(feature) + " 0)"
-                childrenAssert += ")"
+                childrenAssert += ")" # closing ite
                 childrenAssert += create_assert_child_parent_connection(feature.children)
-            childrenAssert += " )"
+            childrenAssert += " )" #closind and
 
     if len(features) != 0:
         if features.__getitem__(0).parent.parent is None:
-            childrenAssert += " )\n"
+            childrenAssert += " )\n" # closing assert
 
     return childrenAssert
 
@@ -204,7 +220,7 @@ def declare_constants(features: list) -> str:
     constants = ""
 
     for feature in features:
-        constants += "(declare-const " + create_const_name(feature) + " int)\n"
+        constants += "(declare-const " + create_const_name(feature) + " Int)\n"
 
     return constants
 
