4615de3f63944fcba37cae8340f10e0e1dc57c4a
upskk <upskk@student.kit.edu>
2025-01-08 14:46:03 +0100

    added model validation

--- cfmtoolbox-csp-encoder/cfmtoolbox_csp_encoder/__init__.py
+++ cfmtoolbox-csp-encoder/cfmtoolbox_csp_encoder/__init__.py
@@ -8,14 +8,20 @@ from cfmtoolbox_csp_encoder.multisetCSP import (create_multiset_csp_encoding,cre
 @app.command()
 def run_csp_solver_maximize_cardinalities(cfm: CFM):
     model = create_multiset_csp_encoding(cfm)
-    '''
+
     variables = get_variables()
+    try:
+        model.Validate()
+        print("Model is valid.")
+    except Exception as e:
+        print(f"Model validation failed: {e}")
+
     solver = cp_model.CpSolver()
     status = solver.solve(model)
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
         for key in variables:
            print(key + ": " + str(solver.Value(variables[key])))
-    '''
+
     find_actual_max(model,cfm.root,1)
 
 
@@ -27,14 +33,14 @@ def find_actual_max(model, feature: Feature, max_parent_cardinality: int):
     model.maximize(variables[create_const_name(feature)])
 
     solver = cp_model.CpSolver()
-    #solver.parameters.log_search_progress = True
-    #solver.parameters.cp_model_presolve = True  # Enable presolve (default)
+    solver.parameters.log_search_progress = True
+    solver.parameters.cp_model_probing_level = 0  # Enable presolve (default)
     #solver.parameters.num_search_workers = 1  # Single-threaded for deterministic debugging
     status = solver.solve(model)
 
 
     if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-
+        #print(solver.objective_value)
         if int(solver.objective_value) > 1:
             actual_max = int(solver.objective_value) / max_parent_cardinality
             new_max = round(max_parent_cardinality * actual_max, None)
@@ -46,11 +52,11 @@ def find_actual_max(model, feature: Feature, max_parent_cardinality: int):
             print("Given feature instance cardinality: " + str(get_max_interval_value(
                 feature.instance_cardinality.intervals)) + "\n")
             print("Actual Max Feature Instance Cardinality " + str(round(actual_max, None)) + "\n")
-        #for key in variables:
-        #    print(key + ": " + str(solver.Value(variables[key])))
+        for key in variables:
+            print(key + ": " + str(solver.Value(variables[key])))
     else:
         print("No solution found.")
-        new_max = 1
+        new_max = 0
 
 
     '''
