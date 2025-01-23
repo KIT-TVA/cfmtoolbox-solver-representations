ae4298596eb5efd7632bd52b6e1b847e3f18ae0c
upskk <upskk@student.kit.edu>
2025-01-23 15:52:07 +0100

    added group cardinalities to cloning encoding

--- cfmtoolbox-csp-encoder/cfmtoolbox_csp_encoder/cloningCSP.py
+++ cfmtoolbox-csp-encoder/cfmtoolbox_csp_encoder/cloningCSP.py
@@ -3,6 +3,7 @@ from cfmtoolbox.plugins.big_m import get_global_upper_bound
 
 from cfmtoolbox_csp_encoder.multisetCSP import create_const_name, \
     create_assert_feature_instance_cardinality, create_assert_group_instance_cardinality
+from cfmtoolbox_smt_encoder.mulitsetSMT import create_amount_of_children_for_group_type_cardinality
 from ortools.sat.python import cp_model
 from ortools.sat.python.cp_model import CpModel
 
@@ -12,7 +13,7 @@ variables = {}
 
 
 
-def create_ilp_cloning_encoding(cfm: CFM,only_boolean_constants: bool):
+def create_csp_cloning_encoding(cfm: CFM,only_boolean_constants: bool):
     """
     :param cfm: An instance of the CFM object which represents a cardinality-based feature model.
                 This model is used as the basis for creating the ILP encoding.
@@ -27,12 +28,16 @@ def create_ilp_cloning_encoding(cfm: CFM,only_boolean_constants: bool):
     print(big_m)
     model = cp_model.CpModel()
     print("Encoding CFM...")
-    declare_cloned_constants(model, cfm.root,[],
+    declare_cloned_constants_csp(model, cfm.root,[],
                                          only_boolean_constants=only_boolean_constants)
     create_assert_feature_instance_cardinality_cloning_csp(model, cfm.root,[],
-                                                           only_boolean_constants=only_boolean_constants)
+                                                         only_boolean_constants=only_boolean_constants)
 
-    create_assert_group_instance_cardinality_cloning_csp(model, cfm.root,[],only_boolean_constants=only_boolean_constants)
+    create_assert_group_cardinality_cloning_csp(model,True, cfm.root,[],
+                                                      only_boolean_constants=only_boolean_constants)
+
+    create_assert_group_cardinality_cloning_csp(model,False, cfm.root,[],
+                                                only_boolean_constants=only_boolean_constants)
 
 
 
@@ -42,45 +47,48 @@ def create_ilp_cloning_encoding(cfm: CFM,only_boolean_constants: bool):
 
 
 
-def declare_cloned_constants(model: CpModel, parent: Feature, parent_list: list[int],
+def declare_cloned_constants_csp(model: CpModel, parent: Feature, parent_list: list[int],
                              only_boolean_constants: bool):
     max = getMaxCardinality(parent.instance_cardinality.intervals)
 
     if parent.parent is None:
         for j in range(1, max + 1):
-            variables[create_const_name(parent)] = model.new_bool_var(create_const_name(parent))
+            name = create_const_name(parent) + "_" + str(j)
+            variables[name] = model.new_bool_var(name)
 
     else:
         # if it is a leave then do not append the instance cardinality, because the last constant will be an integer
         if only_boolean_constants or (not only_boolean_constants and len(parent.children) >= 1):
             parent_list.append(max)
+
+
         # Define the recursive function that generates n nested loops
         def helper(depth, current_indices):
             # Base case: if we've reached the innermost level, execute the code
             if depth == len(parent_list):
-                # decides whether only the constants names are created or the declarations for the encoding
-                    # when it is a leave and not the base cloning approach then add Int datatype, else Bool
-                    if only_boolean_constants or (not only_boolean_constants and len(parent.children) >= 1):
-                        variables[create_const_name(parent)] = model.new_bool_var(
-                            create_const_name(parent))
-                    else:
-                        variables[create_const_name(parent)] = model.new_bool_var(create_const_name(parent))
+                variable_name = create_const_name(parent) + "_" + "_".join(map(str,
+                                                                               current_indices))
+                # when it is a leave and not the base cloning approach then add Int datatype, else Bool
+                if only_boolean_constants or (not only_boolean_constants and len(parent.children) >= 1):
 
-
-            loop_code = ""
+                    variables[variable_name] = model.new_bool_var(variable_name)
+                else:
+                    variables[variable_name] = model.new_int_var(
+                                                        0,big_m,variable_name)
+                return
             # Loop through the range based on the current depth value in arr[depth]
             for i in range(1, parent_list[depth] + 1):  # arr[depth] defines how many times the loop at this level runs
                 current_indices.append(i)  # Add the current index to the list
-                loop_code += helper(depth + 1, current_indices)  # Recurse to the next depth (next loop)
+                helper(depth + 1, current_indices)  # Recurse to the next depth (next loop)
                 current_indices.pop()
-            return loop_code
 
         helper(0, [])
 
 
     for feature in parent.children:
         old_list = parent_list.copy()
-        declare_cloned_constants(model, feature, old_list,only_boolean_constants)
+        declare_cloned_constants_csp(model, feature, old_list,only_boolean_constants)
+
 
 
 
@@ -98,38 +106,37 @@ def create_assert_feature_instance_cardinality_cloning_csp(model, feature,parent
                 interval_literal = model.new_bool_var(
                     "interval_literal_" + str(i) + "_" + feature.name)
                 interval_literals.append(interval_literal)
-                sum_variable = create_sum_of_feature_instance(model, feature, current_indices,
+                sum_variables = create_sum_of_feature_instance(feature, current_indices,
                                                               only_boolean_constants)
                 if feature.parent is not None:
-
                     if len(current_indices) > 0:
                         parent_variable = variables[create_const_name(feature.parent) + "_" + "_".join(
-                            map(str, current_indices)) + " " + str(interval.upper)]
+                            map(str, current_indices))]
                     else:
                         parent_variable = variables[create_const_name(
-                            feature.parent) + "_" + "1" + " " + str(interval.upper)]
+                            feature.parent) + "_" + "1"]
 
-                    model.add(sum_variable >=
+                    model.add(sum(sum_variables) >=
                               interval.lower * parent_variable).only_enforce_if(
                         interval_literal)
                     model.add(
-                        sum_variable <= interval.upper * parent_variable).only_enforce_if(
+                        sum(sum_variables) <= interval.upper * parent_variable).only_enforce_if(
                         interval_literal)
                 else:
                     model.add(
-                        sum_variable >= interval.lower).only_enforce_if(interval_literal)
+                        sum(sum_variables) >= interval.lower).only_enforce_if(interval_literal)
                     model.add(
-                        sum_variable <= interval.upper).only_enforce_if(interval_literal)
+                        sum(sum_variables) <= interval.upper).only_enforce_if(interval_literal)
             model.add_bool_xor(interval_literals)
+            return
 
-        loop_code = ""
         # Loop through the range based on the current depth value in arr[depth]
         for i in range(1, parent_list[
                               depth] + 1):  # arr[depth] defines how many times the loop at this level runs
             current_indices.append(i)  # Add the current index to the list
-            loop_code += helper(depth + 1, current_indices)  # Recurse to the next depth (next loop)
+            helper(depth + 1, current_indices)  # Recurse to the next depth (next loop)
             current_indices.pop()
-        return loop_code
+
 
     helper(0, [])
 
@@ -142,10 +149,8 @@ def create_assert_feature_instance_cardinality_cloning_csp(model, feature,parent
                                                                               only_boolean_constants)
 
 
-def create_sum_of_feature_instance(model: CpModel ,feature: Feature, indices,
-                                   only_boolean_constants:
-bool):
-    upper_bound = big_m * len(feature.children)
+def create_sum_of_feature_instance(feature: Feature, indices,
+                                   only_boolean_constants:bool):
     sum_variables = []
     if only_boolean_constants or (not only_boolean_constants and len(feature.children) >= 1):
         for i in range(1, getMaxCardinality(feature.instance_cardinality.intervals) + 1):
@@ -155,22 +160,70 @@ bool):
             else:
                 sum_variables.append(variables[create_const_name(feature) +  "_" +
                                                      str(i)])
-
     else:
        sum_variables.append(variables[create_const_name(feature) + "_" + "_".join(map(str, indices))])
 
-    total_sum_variables = model.new_int_var(0, upper_bound, "total_sum_variables_" + str(
-            feature.name))
-    model.add(total_sum_variables == sum(sum_variables))
 
-
-    return total_sum_variables
+    return sum_variables
 
 
 
-def create_assert_group_instance_cardinality_cloning_csp(model: CpModel, feature: Feature, parent_list: list[int],
-                                                         only_boolean_constants: bool):
+def create_assert_group_cardinality_cloning_csp(model: CpModel,instance: bool, feature: Feature,
+                                                parent_list: list[int], only_boolean_constants: bool):
 
+    if len(feature.children) != 0:
+        if feature.parent is not None:
+            max_cardinality = getMaxCardinality(feature.instance_cardinality.intervals)
+            parent_list.append(max_cardinality)
+
+        # Define the recursive function that generates n nested loops
+        def helper(depth, current_indices):
+
+            # Base case: if we've reached the innermost level, execute the code
+            if depth == len(parent_list):
+
+                if instance:
+                    interval = feature.group_instance_cardinality.intervals.__getitem__(0)
+                    sum_variables = create_amount_of_children_for_group_instance_cardinality_cloning_csp(
+                        feature.children, current_indices, only_boolean_constants)
+                else:
+                    interval = feature.group_type_cardinality.intervals.__getitem__(0)
+                    sum_variables = create_amount_of_children_for_group_type_cardinality_cloning_csp(
+                        model, feature.children, current_indices, only_boolean_constants)
+
+                if feature.parent is not None:
+
+                    if len(current_indices) > 0:
+                        parent_variable = variables[create_const_name(feature) + "_" + "_".join(
+                            map(str, current_indices))]
+                    else:
+                        parent_variable = variables[create_const_name(
+                            feature.parent) + "_" + "1"]
+                    model.add(sum(sum_variables) >=
+                              interval.lower *
+                              parent_variable)
+                    model.add(
+                        sum(sum_variables) <= interval.upper * parent_variable)
+                else:
+                    model.add(sum(sum_variables) >=
+                              interval.lower)
+                    model.add(
+                        sum(sum_variables) <= interval.upper)
+                return
+
+            # Loop through the range based on the current depth value in arr[depth]
+            for i in range(1, parent_list[depth] + 1):  # arr[depth] defines how many times the loop at this level runs
+                current_indices.append(i)  # Add the current index to the list
+                helper(depth + 1, current_indices)  # Recurse to the next depth (next loop)
+                current_indices.pop()
+
+        helper(0, [])
+
+
+        for child in feature.children:
+            old_list = parent_list.copy()
+            create_assert_group_cardinality_cloning_csp(model, instance, child, old_list,
+                                                                                  only_boolean_constants)
 
 
 def getMaxCardinality(intervals: list[Interval]) -> int:
@@ -180,3 +233,56 @@ def getMaxCardinality(intervals: list[Interval]) -> int:
             max = interval.upper
 
     return max
+
+
+def create_amount_of_children_for_group_instance_cardinality_cloning_csp(children:
+                                                                list[Feature], indices, only_boolean_constants):
+
+    sum_variables = []
+
+    for feature in children:
+        if only_boolean_constants or (not only_boolean_constants and len(feature.children) >= 1):
+            for i in range(1, getMaxCardinality(feature.instance_cardinality.intervals) + 1):
+                if len(indices) > 0:
+                    sum_variables.append(variables[create_const_name(feature) + "_" +  "_".join(map(str, indices)) + "_" + str(i)])
+                else:
+                    sum_variables.append(variables[create_const_name(feature) + "_" + str(i)])
+        else:
+            sum_variables.append(variables[create_const_name(feature) + "_" + "_".join(map(str, indices))])
+
+
+    return sum_variables
+
+def create_amount_of_children_for_group_type_cardinality_cloning_csp(model: CpModel, children:
+                                                                list[Feature], indices, only_boolean_constants):
+    sum_variables = []
+
+    for feature in children:
+        if only_boolean_constants or (not only_boolean_constants and len(feature.children) >= 1):
+            helper_variables = []
+            for i in range(1, getMaxCardinality(feature.instance_cardinality.intervals) + 1):
+                helper_variable = model.new_bool_var(create_const_name(feature) + "_" +
+                                                     "_".join(map(str, indices)) + "_" + str(
+                                                     i) + "_helper")
+                helper_variables.append(helper_variable)
+                if len(indices) > 0:
+                    variable_name = create_const_name(feature) + "_" + "_".join(
+                        map(str, indices)) + "_" + str(i)
+                    model.add(variables[variable_name] > 0).only_enforce_if(helper_variable)
+                    model.add(variables[variable_name] <= 0).only_enforce_if(helper_variable.Not())
+                else:
+                    variable_name = create_const_name(feature) + "_" + str(i)
+                    model.add(variables[variable_name] > 0).only_enforce_if(helper_variable)
+                    model.add(variables[variable_name] <= 0).only_enforce_if(helper_variable.Not())
+
+            helper2 = model.new_bool_var(create_const_name(feature) + "_" +
+                                                     "_".join(map(str, indices)) + "_helper2")
+            model.add_at_least_one(helper_variables).only_enforce_if(helper2)
+            model.add(sum(helper_variables) <= 0).only_enforce_if(helper2.Not())
+            sum_variables.append(helper2)
+
+        else:
+            sum_variables.append(create_const_name(feature) + "_" + "_".join(map(str, indices)))
+
+
+    return sum_variables
