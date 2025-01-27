609e28c3a12e9df07c0b117257802fdbb18c9aec
upskk <upskk@student.kit.edu>
2025-01-27 12:13:30 +0100

    fixed group type constraint for integer leafes app

--- cfmtoolbox-csp-encoder/cfmtoolbox_csp_encoder/cloningCSP.py
+++ cfmtoolbox-csp-encoder/cfmtoolbox_csp_encoder/cloningCSP.py
@@ -1,4 +1,4 @@
-from cfmtoolbox import CFM, Feature, Interval
+from cfmtoolbox import CFM, Feature, Interval, Constraint
 from cfmtoolbox.plugins.big_m import get_global_upper_bound
 
 from cfmtoolbox_csp_encoder.multisetCSP import create_const_name, \
@@ -9,6 +9,7 @@ from ortools.sat.python.cp_model import CpModel
 
 big_m = 0
 variables = {}
+
 
 
 
@@ -39,6 +40,9 @@ def create_csp_cloning_encoding(cfm: CFM,only_boolean_constants: bool):
     create_assert_group_cardinality_cloning_csp(model,False, cfm.root,[],
                                                 only_boolean_constants=only_boolean_constants)
 
+
+    create_assert_constraints_cloning_csp(model,cfm.constraints,cfm.root,
+        only_boolean_constants=only_boolean_constants)
 
 
     print("Encoding complete.")
@@ -161,7 +165,7 @@ def create_sum_of_feature_instance(feature: Feature, indices,
                 sum_variables.append(variables[create_const_name(feature) +  "_" +
                                                      str(i)])
     else:
-       sum_variables.append(variables[create_const_name(feature) + "_" + "_".join(map(str, indices))])
+        sum_variables.append(variables[create_const_name(feature) + "_" + "_".join(map(str, indices))])
 
 
     return sum_variables
@@ -190,6 +194,8 @@ def create_assert_group_cardinality_cloning_csp(model: CpModel,instance: bool, f
                     interval = feature.group_type_cardinality.intervals.__getitem__(0)
                     sum_variables = create_amount_of_children_for_group_type_cardinality_cloning_csp(
                         model, feature.children, current_indices, only_boolean_constants)
+
+
 
                 if feature.parent is not None:
 
@@ -249,7 +255,7 @@ def create_amount_of_children_for_group_instance_cardinality_cloning_csp(childre
                     sum_variables.append(variables[create_const_name(feature) + "_" + str(i)])
         else:
             sum_variables.append(variables[create_const_name(feature) + "_" + "_".join(map(str, indices))])
-
+            #print(variables[create_const_name(feature) + "_" + "_".join(map(str, indices))])
 
     return sum_variables
 
@@ -282,7 +288,114 @@ def create_amount_of_children_for_group_type_cardinality_cloning_csp(model: CpMo
             sum_variables.append(helper2)
 
         else:
-            sum_variables.append(create_const_name(feature) + "_" + "_".join(map(str, indices)))
-
+            feature_int_variable = variables[create_const_name(feature) + "_" + "_".join(map(str,
+                                                                                      indices))]
+            helper_int = model.new_bool_var(create_const_name(feature) + "_" +
+                                         "_".join(map(str, indices)) + "_helper_int")
+            model.add(feature_int_variable > 0).only_enforce_if(helper_int)
+            model.add(feature_int_variable <= 0).only_enforce_if(helper_int.Not())
+            sum_variables.append(helper_int)
 
     return sum_variables
+
+
+def create_assert_constraints_cloning_csp(model: CpModel, constraints: list[Constraint],
+                                          root: Feature,
+                                          only_boolean_constants):
+    for i, constraint in enumerate(constraints):
+        first_interval_constant = model.new_bool_var("constraint_first_interval_literal_" +
+                                                     str(i))
+        first_interval = constraint.first_cardinality.intervals.__getitem__(0)
+
+        variables["constraint_first_interval_literal_" +
+                  str(i)] = first_interval_constant
+
+        sum_variables = create_constraint_feature_to_intervals_cloning(root,
+                                                                       constraint.first_feature,only_boolean_constants)
+
+        model.add(sum(sum_variables) >=
+                  first_interval.lower).only_enforce_if(first_interval_constant)
+        model.add(sum(sum_variables) <=
+                  first_interval.upper).only_enforce_if(first_interval_constant)
+
+        out_of_range_low = model.NewBoolVar('out_of_range_low')
+        out_of_range_high = model.NewBoolVar('out_of_range_high')
+
+        # Define the conditions for the OR
+        model.Add(sum(sum_variables) < first_interval.lower).only_enforce_if(out_of_range_low)
+        model.Add(sum(sum_variables) > first_interval.upper).only_enforce_if(out_of_range_high)
+
+        model.AddBoolOr([out_of_range_low, out_of_range_high]).only_enforce_if(
+            first_interval_constant.Not())
+
+        second_interval_constant = model.new_bool_var("constraint_second_interval_literal_" +
+                                                      str(i))
+        variables["constraint_second_interval_literal_" +
+                  str(i)] = second_interval_constant
+
+        sum_variables2 = create_constraint_feature_to_intervals_cloning(root,
+                                                                       constraint.second_feature,
+                                                                        only_boolean_constants)
+
+        second_interval = constraint.second_cardinality.intervals.__getitem__(0)
+        model.add(sum(sum_variables2) >=
+                  second_interval.lower).only_enforce_if(second_interval_constant)
+        model.add(sum(sum_variables2) <= second_interval.upper).only_enforce_if(
+            second_interval_constant)
+
+        out_of_range_low_2 = model.NewBoolVar('out_of_range_low')
+        out_of_range_high_2 = model.NewBoolVar('out_of_range_high')
+
+        model.Add(sum(sum_variables2) < second_interval.lower).only_enforce_if(out_of_range_low_2)
+        model.Add(sum(sum_variables2) > second_interval.upper).only_enforce_if(
+            out_of_range_high_2)
+
+        model.AddBoolOr([out_of_range_low_2, out_of_range_high_2]).only_enforce_if(
+            second_interval_constant.Not())
+
+        if constraint.require:
+            model.add_implication(first_interval_constant, second_interval_constant)
+        else:
+            model.add(first_interval_constant + second_interval_constant <= 1)
+
+
+def create_constraint_feature_to_intervals_cloning(root: Feature,
+                                                   feature: Feature,only_boolean_constants: bool):
+
+
+    parent_list = create_parent_list_for_feature_by_name(root, feature.name, [])
+    sum_variables = []
+    # Define the recursive function that generates n nested loops
+    def helper(depth, current_indices):
+        asserts = ""
+        # Base case: if we've reached the innermost level, execute the code
+        if depth == len(parent_list):
+            for variable in create_sum_of_feature_instance(feature, indices=current_indices,
+                                            only_boolean_constants=only_boolean_constants):
+                sum_variables.append(variable)
+            return asserts
+
+        loop_code = ""
+        # Loop through the range based on the current depth value in arr[depth]
+        for i in range(1, parent_list[depth] + 1):  # arr[depth] defines how many times the loop at this level runs
+            current_indices.append(i)  # Add the current index to the list
+            loop_code += helper(depth + 1, current_indices)  # Recurse to the next depth (next loop)
+            current_indices.pop()
+        return loop_code
+
+    helper(0, [])
+
+    return sum_variables
+
+
+def create_parent_list_for_feature_by_name(feature: Feature, feature_name: str, parent_list: list[int]):
+    if feature.name is feature_name:
+        return parent_list
+    else:
+        if feature.parent is not None:
+            parent_list.append(getMaxCardinality(feature.instance_cardinality.intervals))
+        for child in feature.children:
+            old_list = parent_list.copy()
+            new_list = create_parent_list_for_feature_by_name(child, feature_name,old_list)
+            if new_list is not None:
+                return new_list
